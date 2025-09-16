8b45750db8b453e8d4456d49037bf3dfb4660be4
Fabiovgc <fabiovgc@gmail.com>
2025-09-15 21:39:06 -0300

    finish 'JobsDbContext'

--- jobs-api/jobs-api/Persistency/JobsDbContext.cs
+++ jobs-api/jobs-api/Persistency/JobsDbContext.cs
@@ -1,11 +1,26 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using jobs_api.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography.X509Certificates;
 
 namespace jobs_api.Persistency
 {
     public class JobsDbContext : DbContext
     {
         public JobsDbContext(DbContextOptions<JobsDbContext> options) : base(options)
+        { }
+
+        // DB Sets
+        public DbSet<Job> Jobs { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
         {
+            // If you want to configure the Job entity, use modelBuilder.Entity<Job>()
+            // For example, to configure the table name:
+            builder.Entity<Job>(o => 
+            {
+                o.HasKey(j => j.Id);
+            });
         }
-}
+
+    }
 }
